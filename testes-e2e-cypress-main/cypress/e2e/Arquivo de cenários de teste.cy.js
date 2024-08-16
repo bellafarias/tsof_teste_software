b0ad1dcac597d4b233cd261fdb7d56ce618c0251
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-16 11:44:13 -0300

    realização do teste de texto, e de imagens

--- testes-e2e-cypress-main/cypress/e2e/Arquivo de cenários de teste.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/Arquivo de cenários de teste.cy.js
@@ -9,14 +9,48 @@ describe('template spec', () => {
       cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
     })
   })
-  describe('Testes', () => {
+  describe('Testes de texto', () => {
+  it('Deve renderizar h2-vantagem com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('vantagem').contains('Vantagens do nosso banco:')
+  })
   it('Deve renderizar h3-anuncio com o texto correto!', () => {
     cy.visit('http://localhost:3000')
     cy.getByData('anuncio').contains('Conta e cartão gratuitos!')
   })
+  it('Deve renderizar paragrafo com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('texto').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+  })
+  it('Deve renderizar h3-saque com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('saque').contains('Saques sem custo')
+  })
+  it('Deve renderizar paragrafo 02 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('texto2').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+  })
+  it('Deve renderizar h3-pontos com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('pontos').contains('Programa de pontos')
+  })
+  it('Deve renderizar paragrafo 03 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('texto3').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+  })
   it('Deve renderizar h3-seguro com o texto correto!', () => {
     cy.visit('http://localhost:3000')
     cy.getByData('seguro').contains('Seguro de onde estiver!')
+  })
+  it('Deve renderizar paragrafo 04 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.getByData('texto4').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+  })
+})
+    describe('Testes das Imagens', () => {
+    it('Deve renderizar Todas as Imagens corretamente!', () => {
+      cy.visit('http://localhost:3000')
+      cy.getByData('imagens').should("be.visible")
   })
 })
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -16,47 +16,54 @@ export default function Inicio() {
               Experimente mais liberdade no controle da sua vida financeira.
               Crie sua conta com a gente!
             </h1>
-            <img
+            <img data-test="imagens"
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
           </div>
           <div className={estilos.vantagens}>
-            <h2>Vantagens do nosso banco:</h2>
+            <h2 data-test="vantagem">
+              Vantagens do nosso banco:
+            </h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
-                <img src={presente} alt="ícone de um presente" />
+                <img data-test="imagens" src={presente} alt="ícone de um presente" />
                 <h3 data-test="anuncio">
                   Conta e cartão gratuitos!
                 </h3>
-                <p>
+                <p data-test="texto">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+                <img data-test="imagens" src={saque} alt="ícone de uma mão segurando um dinheiro" />
+                <h3 data-test="saque">
+                  Saques sem custo
+                  </h3>
+                <p data-test="texto2">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
-                <p>
+                <img data-test="imagens" src={estrela} alt="ícone de uma estrela" />
+                <h3 data-test="pontos">
+                  Programa de pontos
+                  </h3>
+                <p data-test="texto3">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
                 <img
+                  data-test="imagens"
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
                 <h3 data-test="seguro">Seguro de onde estiver!</h3>
-                <p>
+                <p data-test="texto4">
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
