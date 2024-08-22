bf73f259941d734d90f563d27db3c79b2e81f62d
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-22 14:38:27 -0300

    campos obrigatórios testes concluidos

--- testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioCadastro.cy.js
@@ -4,35 +4,35 @@ describe('Formulário cadastro', () => {
     })
     it('Usuário deve conseguir se cadastrar com sucesso', () => {
     cy.getByData('botao-cadastro').click() 
-    cy.getByData('nome-input').type('Lorraine')
-    cy.getByData('email-input').type('Lorraine@gemail.com10')
+    cy.getByData('nome-input').type('leticia')
+    cy.getByData('email-input').type('leticia@gemail.com10')
     cy.getByData('senha-input').type('01234567')
     cy.getByData('botao-enviar').click()
     cy.getByData('mensagem-sucesso').should('exist').and('have.text', 'Usuário cadastrado com sucesso!')})
 
-//     // it('Não deve permitir o campo nome em branco', () => {
-//     //         cy.getByData('botao-cadastro').click()
-//     //         cy.getByData('email-input').type('luana@gemail.com10')
-//     //         cy.getByData('senha-input').type('3456789')
-//     //         cy.getByData('botao-enviar').click()
-//     //         cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
-//     //     })
+    it('Não deve permitir o campo nome em branco', () => {
+            cy.getByData('botao-cadastro').click()
+            cy.getByData('email-input').type('leticia@gemail.com10')
+            cy.getByData('senha-input').type('3456789')
+            cy.getByData('botao-enviar').click()
+            cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+        })
     
-//     //     it('Não deve permitir o campo email em branco', () => {
-//     //         cy.visit('http://localhost:3000')
-//     //         cy.getByData('botao-cadastro').click()
-//     //         cy.getByData('senha-input').type('3456789')
-//     //         cy.getByData('botao-enviar').click()
-//     //         cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
-//     //     })
+        it('Não deve permitir o campo email em branco', () => {
+            cy.visit('http://localhost:3000')
+            cy.getByData('botao-cadastro').click()
+            cy.getByData('senha-input').type('3456789')
+            cy.getByData('botao-enviar').click()
+            cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
+        })
     
-//     //     it('Não deve permitir o campo senha em branco', () => {
-//     //         cy.visit('http://localhost:3000')
-//     //         cy.getByData('botao-cadastro').click()
-//     //         cy.getByData('email-input').type('luana@gemail.com10')
-//     //         cy.getByData('botao-enviar').click()
-//     //         cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório')
-//         // })
+        it('Não deve permitir o campo senha em branco', () => {
+            cy.visit('http://localhost:3000')
+            cy.getByData('botao-cadastro').click()
+            cy.getByData('email-input').type('leticia@gemail.com10')
+            cy.getByData('botao-enviar').click()
+            cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório')
+        })
 
 //         it('Não deve permitir um e-mail inválido!', () => {
 //             cy.getByData('botao-cadastro').click()
