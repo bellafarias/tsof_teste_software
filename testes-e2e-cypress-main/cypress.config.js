4920177772e512fc04c82b57035ec9195d63a2b9
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-22 08:47:25 -0300

    validação de página de login e cartão

--- testes-e2e-cypress-main/cypress.config.js
+++ testes-e2e-cypress-main/cypress.config.js
@@ -4,6 +4,11 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+
     },
   },
+});
+module.exports = defineConfig({
+  e2e: {
+  baseUrl: 'http://localhost:3000' }, 
 });
