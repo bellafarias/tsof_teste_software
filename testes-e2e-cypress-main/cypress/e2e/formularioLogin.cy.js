4e7e511af76038a418e420ae38d7c7af0fed6901
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-23 14:02:25 -0300

    Permissão de transação e logout bancário

--- testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formularioLogin.cy.js
@@ -1,17 +1,16 @@
 describe('Formulário de login', () => { 
     beforeEach(()=> {
-    cy.visit('http://localhost:3000')})
-
-    it('Não deve permitir um e-mail inválido!', () => {
-    cy.getByData('botao-login').click()
-    cy.getByData('email-input').type('neilton@alura')
-    cy.getByData('senha-input').type('123456')
-    cy.getByData('botao-enviar').click()
-    cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+    cy.visit('/')})
+        it('Não deve permitir um e-mail inválido!', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
     })
 
     it('Não deve permitir o campo email em branco', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
         cy.getByData('botao-login').click()
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
@@ -19,14 +18,10 @@ describe('Formulário de login', () => {
     })
 
     it('Não deve permitir o campo senha em branco', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
         cy.getByData('botao-login').click()
         cy.getByData('email-input').type('neilton@alura.com')
         cy.getByData('botao-enviar').click()
         cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório')
     })
 })
-
-    
-
-    
