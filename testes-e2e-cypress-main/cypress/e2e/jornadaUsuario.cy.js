7f3e1a9049f93c0709ca22f2d38b5fd7562aa17a
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-23 14:03:02 -0300

    Permissão de depósito  e logout bancário

--- testes-e2e-cypress-main/cypress/e2e/jornadaUsuario.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/jornadaUsuario.cy.js
@@ -1,5 +1,21 @@
 describe('Jornadas de usuário', () => {
 
+    it('Deve permitir que o usuário acesse a aplicação, realize um depósito e faça logout', () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.location('pathname').should('eq','/home')
+    cy.getByData('select-opcoes').select('Depósito')
+    
+    cy.getByData('form-input').type('200') 
+    cy.getByData('realiza-transacao').click()
+    
+    cy.getByData('lista-transacoes').find('li').last().contains('R$ 200') 
+    cy.getByData('botao-sair').click()
+    cy.location('pathname').should('eq','/')
+    })
 
     it('Deve permitir que o usuário acesse a aplicação, realize uma transação e faça logout', () => {
     cy.visit('/')
