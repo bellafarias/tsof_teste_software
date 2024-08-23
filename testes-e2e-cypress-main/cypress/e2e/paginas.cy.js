63e9592ea2c847e49dcfc1ce26665cb4d12f7d14
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-23 09:27:19 -0300

    teste de multiplas páginas

---
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -0,0 +1,34 @@
+describe('Testando múltiplas páginas', () => {
+    it('Deve conseguir acessar a página de cartões', () => {
+    cy.visit('/')})
+    })
+
+// describe('Testando múltiplas páginas', () => {
+//     beforeEach('Fazer sempre antes dos testes', () => {
+//         cy.visit('/')
+//         cy.getByData('botao-login').click()
+//         cy.getByData('email-input').type('neilton@alura.com')
+//         cy.getByData('senha-input').type('123456')
+//         cy.getByData('botao-enviar').click()
+//     })
+
+//     it('Deve conseguir acessar a página de Inicio', () => {
+//         cy.getByData('app-home').find('a').eq(0).click() 
+//         cy.getByData('title-one').should('exist').and('have.text','Nova Transação')
+//     })
+
+//     it('Deve conseguir acessar a página de cartões', () => {
+//     cy.getByData('app-home').find('a').eq(1).click() 
+//     cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
+//     })
+
+//     it('Deve conseguir acessar a página de Serviços', () => {
+//         cy.getByData('app-home').find('a').eq(2).click() 
+//         cy.getByData('servicos').should('exist').and('have.descendants','h5', 'img')
+//     })
+
+//     it('Deve conseguir acessar a página de Investimentos', () => {
+//         cy.getByData('app-home').find('a').eq(3).click() 
+//         cy.getByData('invest').should('exist').and('have.text','Investimentos')
+//     })
+// })
