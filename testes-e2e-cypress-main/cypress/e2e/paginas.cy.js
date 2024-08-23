31c3381971457c6c89bef50d303520d84b6125b3
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-23 09:27:46 -0300

    teste dos itens no menu lateral

--- testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/paginas.cy.js
@@ -3,32 +3,32 @@ describe('Testando múltiplas páginas', () => {
     cy.visit('/')})
     })
 
-// describe('Testando múltiplas páginas', () => {
-//     beforeEach('Fazer sempre antes dos testes', () => {
-//         cy.visit('/')
-//         cy.getByData('botao-login').click()
-//         cy.getByData('email-input').type('neilton@alura.com')
-//         cy.getByData('senha-input').type('123456')
-//         cy.getByData('botao-enviar').click()
-//     })
+describe('Testando múltiplas páginas', () => {
+    beforeEach('Fazer sempre antes dos testes', () => {
+        cy.visit('/')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+    })
 
-//     it('Deve conseguir acessar a página de Inicio', () => {
-//         cy.getByData('app-home').find('a').eq(0).click() 
-//         cy.getByData('title-one').should('exist').and('have.text','Nova Transação')
-//     })
+    it('Deve conseguir acessar a página de Inicio', () => {
+        cy.getByData('app-home').find('a').eq(0).click() 
+        cy.getByData('title-one').should('exist').and('have.text','Nova Transação')
+    })
 
-//     it('Deve conseguir acessar a página de cartões', () => {
-//     cy.getByData('app-home').find('a').eq(1).click() 
-//     cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
-//     })
+    it('Deve conseguir acessar a página de cartões', () => {
+    cy.getByData('app-home').find('a').eq(1).click() 
+    cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
+    })
 
-//     it('Deve conseguir acessar a página de Serviços', () => {
-//         cy.getByData('app-home').find('a').eq(2).click() 
-//         cy.getByData('servicos').should('exist').and('have.descendants','h5', 'img')
-//     })
+    it('Deve conseguir acessar a página de Serviços', () => {
+        cy.getByData('app-home').find('a').eq(2).click() 
+        cy.getByData('servicos').should('exist').and('have.descendants','h5', 'img')
+    })
 
-//     it('Deve conseguir acessar a página de Investimentos', () => {
-//         cy.getByData('app-home').find('a').eq(3).click() 
-//         cy.getByData('invest').should('exist').and('have.text','Investimentos')
-//     })
-// })
+    it('Deve conseguir acessar a página de Investimentos', () => {
+        cy.getByData('app-home').find('a').eq(3).click() 
+        cy.getByData('invest').should('exist').and('have.text','Investimentos')
+    })
+})
