6cfffa8daab982703d87cb87ed8d9a576ed30b25
bellafarias <pereira.isabellafarias@gmail.com>
2024-08-29 10:56:38 -0300

    alteração da view para Desktop

--- testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/dispositivosMoveis.cy.js
@@ -1,17 +1,47 @@
-describe('Testando dispositivos móveis', () => {
-    it('Deve existir um menu burguer', () => {
-        cy.visit('/')
-        
-        cy.getByData('botao-login').click()
-        cy.getByData('email-input').type('neilton@alura.com')
-        cy.getByData('senha-input').type('123456')
-        cy.getByData('botao-enviar').click()
-        
-        cy.location('pathname').should('eq','/home')
-
-        cy.getByData('menu-burguer').click()
-        cy.getByData('menu-lateral').find('a').eq(3).click()			
-
-        cy.location('pathname').should('eq','/home/investimentos')			
+describe('Menu de navegação', () => {
+    context('Resolução de 720p', () => {
+      beforeEach(() => {
+        cy.viewport(1280, 720)
+      })
+      describe('Testando dispositivos móveis', () => {
+        it('Deve existir um menu burguer', () => {
+            cy.visit('/')
+            
+            cy.getByData('botao-login').click()
+            cy.getByData('email-input').type('neilton@alura.com')
+            cy.getByData('senha-input').type('123456')
+            cy.getByData('botao-enviar').click()
+            
+            cy.location('pathname').should('eq','/home')
+    
+            cy.getByData('menu-burguer').click()
+            cy.getByData('menu-lateral').find('a').eq(3).click()			
+    
+            cy.location('pathname').should('eq','/home/investimentos')			
+        })
     })
+    })
+
+// context('Resolução do iphone-5 ', () => {
+//     beforeEach(() => {
+//         cy.viewport('iphone-5', 'landscape')
+//     })
+//     describe('Testando dispositivos móveis', () => {
+//         it('Deve existir um menu burguer', () => {
+//             cy.visit('/')
+            
+//             cy.getByData('botao-login').click()
+//             cy.getByData('email-input').type('neilton@alura.com')
+//             cy.getByData('senha-input').type('123456')
+//             cy.getByData('botao-enviar').click()
+            
+//             cy.location('pathname').should('eq','/home')
+
+//             cy.getByData('menu-burguer').click()
+//             cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+//             cy.location('pathname').should('eq','/home/investimentos')			
+//         })
+//     })
+// })
 })
